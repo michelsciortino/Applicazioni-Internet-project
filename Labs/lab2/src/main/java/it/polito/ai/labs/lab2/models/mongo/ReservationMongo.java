581d56d22e5194c792a57166600594cdac1c9205
MoraAndrea <s255280@studenti.polito.it>
2019-04-17 11:07:36 +0200

    fixed compoundindex problem

--- Labs/lab2/src/main/java/it/polito/ai/labs/lab2/models/mongo/ReservationMongo.java
+++ Labs/lab2/src/main/java/it/polito/ai/labs/lab2/models/mongo/ReservationMongo.java
@@ -6,6 +6,7 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Reference;
 import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.CompoundIndexes;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -14,11 +15,12 @@ import java.time.LocalDateTime;
 
 @Document(collection = "reservations")
 @Data
-//@CompoundIndex(name = "reservationID",unique = true,def = "{'userID':1,'childName':1,'data':1")
 @Builder
+@CompoundIndex(def = "{'childName':1, 'userID':1, 'data':1}",unique = true, name = "reservationIndex")
 public class ReservationMongo {
     @Id
     private ObjectId id;
+
     private String userID;
     private String childName;
     private String lineName;
