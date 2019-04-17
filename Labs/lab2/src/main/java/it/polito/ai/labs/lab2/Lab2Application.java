5a106167e0d4013f876297ad28dbcffc5ae7e204
Raffaele <raffyt@hotmail.it>
2019-04-17 11:35:10 +0200

    added reading json files either from files or folder

--- Labs/lab2/src/main/java/it/polito/ai/labs/lab2/Lab2Application.java
+++ Labs/lab2/src/main/java/it/polito/ai/labs/lab2/Lab2Application.java
@@ -14,8 +14,11 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import javax.validation.constraints.Null;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class Lab2Application implements CommandLineRunner {
@@ -36,30 +39,70 @@ public class Lab2Application implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
-        for (String filename : config.getFiles()) {
-            InsertLineFromFile(filename);
+    public void run(String[] args) throws Exception {
+        for (String arg : args) {
+            if (arg.startsWith("--files=")) {
+                config.setFileNames(arg);
+                for (String filename : config.getFileNames()) {
+                    InsertLineFromFile(filename);
+                }
+            }
+            else if (arg.startsWith("--folder=")) {
+                config.setFolderName(arg);
+                InsertLineFromFileInFolder(config.getFolderName());
+            }
+
+        }
+
+    }
+
+    public void InsertLineFromFileInFolder(String foldername) throws IOException {
+        File folder = new ClassPathResource(foldername).getFile();
+        for (File f : folder.listFiles()) {
+            InsertLineFromFile("/JsonFiles/" + f.getName());
         }
     }
 
     public void InsertLineFromFile(String filename) throws IOException {
-        File file = new ClassPathResource("JsonFiles").getFile();
-        Line line = deserializer.readLinesFromJsonFile(file.toPath());
-        database.insertLine(line);
+
+        File file = new ClassPathResource(filename).getFile();
+        Line line;
+        if (file.exists()) {
+            line = deserializer.readLinesFromJsonFile(file.toPath());
+            database.insertLine(line);
+        } else throw new IOException("One of files doesn't exist");
     }
 
     @Configuration
     @EnableConfigurationProperties
     public class ApplicationConfig {
-        private String[] files;
+        public List<String> filenames;
+        public String foldername;
 
-        public String[] getFiles() {
-            return files;
+        public String getFolderName() {
+            return foldername;
         }
 
-        public void setFiles(String[] files) {
-            this.files = files;
+        public List<String> getFileNames() {
+            return filenames;
+        }
+
+        public void setFileNames(String arg) {
+            filenames = new ArrayList<>();
+            String[] splitted = arg.split("=");
+            if (splitted.length < 2)
+                return;
+            String[] fileList = splitted[1].split(",");
+            for (int i = 0, size = fileList.length; i < size; i++) {
+                filenames.add(fileList[i]);
+            }
+        }
+
+        public void setFolderName(String arg) {
+            String[] splitted = arg.split("=");
+            if (splitted.length < 2)
+                return;
+            foldername = splitted[1];
         }
     }
-
 }
