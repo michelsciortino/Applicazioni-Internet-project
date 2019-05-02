087642cb6f0496049a89370b348cbfc6a2d3e9ee
michelsciortino <michel.sciortino@outlook.com>
2019-05-02 18:36:59 +0200

    Account Roles defined.

---
+++ Labs/lab3/src/main/java/it/polito/ai/labs/lab3/authentication/AccountRoles.java
@@ -0,0 +1,10 @@
+package it.polito.ai.labs.lab3.authentication;
+
+public class AccountRoles {
+    public static final String ADMINISTRATOR = "admin";
+    public static final String ACCOMPANIER = "accompanier";
+    public static final String ADULT = "adult";
+    public static final String CHILD = "child";
+    public static final String PASSENGER = "passenger";
+    public static final String MASTER = "master";
+}
