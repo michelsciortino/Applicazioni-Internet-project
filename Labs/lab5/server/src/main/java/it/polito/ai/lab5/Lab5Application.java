63438f5046def633d3724e4021e73f4886a6c19b
MoraAndrea <s255280@studenti.polito.it>
2019-06-09 00:24:15 +0200

    initial commit server

---
+++ Labs/lab5/server/src/main/java/it/polito/ai/lab5/Lab5Application.java
@@ -0,0 +1,13 @@
+package it.polito.ai.lab5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab5Application.class, args);
+    }
+
+}
