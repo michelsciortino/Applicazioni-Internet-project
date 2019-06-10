3c7b632eccb6bb25d48aea0bd9bf96a8f49136de
MoraAndrea <s255280@studenti.polito.it>
2019-06-11 00:14:46 +0200

    add class for direction type

---
+++ Labs/lab5/server/src/main/java/it/polito/ai/lab5/controllers/models/DirectionType.java
@@ -0,0 +1,6 @@
+package it.polito.ai.lab5.controllers.models;
+
+public class DirectionType {
+    public static final String OUTWARD = "outward";
+    public static final String RETURN = "return";
+}
