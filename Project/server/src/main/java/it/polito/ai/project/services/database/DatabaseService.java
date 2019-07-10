ce2ea0d4cb8590fe24c3a9aa53c9ec08a21ec221
MoraAndrea <s255280@studenti.polito.it>
2019-07-11 00:00:18 +0200

    add repositories to DatabaseService

--- Project/server/src/main/java/it/polito/ai/project/services/database/DatabaseService.java
+++ Project/server/src/main/java/it/polito/ai/project/services/database/DatabaseService.java
@@ -1,4 +1,20 @@
 package it.polito.ai.project.services.database;
 
-public class DatabaseService {
+import it.polito.ai.project.services.database.repositories.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class DatabaseService implements DatabaseServiceInterface {
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    private LineRepository lineRepository;
+    @Autowired
+    private RaceRepository raceRepository;
+    @Autowired
+    private UserCredentialsRepository userCredentialsRepository;
+    @Autowired
+    private TokenRepository tokenRepository;
+    @Autowired
+    private UserRepository userRepository;
 }
