de3fbe83f4077c732e8ebb5cf1b91c5bb897da33
Emanuele <emanuele.virgillito@libero.it>
2019-07-12 20:05:52 +0200

    Repository: Queries Added

--- Project/server/src/main/java/it/polito/ai/project/services/database/repositories/UserRepository.java
+++ Project/server/src/main/java/it/polito/ai/project/services/database/repositories/UserRepository.java
@@ -1,10 +1,14 @@
 package it.polito.ai.project.services.database.repositories;
 
 import it.polito.ai.project.services.database.models.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByUsername(String username);
+
+    Page<User> findAll(Pageable pageable);
 }
