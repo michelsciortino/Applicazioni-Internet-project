7b6fa6a50bb2d1118004ef28d5161b1e82a05ae4
MoraAndrea <s255280@studenti.polito.it>
2019-07-14 02:19:56 +0200

    Change class ScopeToken in enum

--- Project/server/src/main/java/it/polito/ai/project/services/database/models/ScopeToken.java
+++ Project/server/src/main/java/it/polito/ai/project/services/database/models/ScopeToken.java
@@ -1,6 +1,6 @@
 package it.polito.ai.project.services.database.models;
 
-public class ScopeToken {
-    public static final String CONFIRM = "confirm";
-    public static final String RECOVERY = "recovery";
+public enum ScopeToken {
+    CONFIRM,
+    RECOVERY
 }
