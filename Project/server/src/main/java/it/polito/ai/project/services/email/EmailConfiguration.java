de1ecc51115d86a5acdeb529290c137e0b9cdca4
MoraAndrea <s255280@studenti.polito.it>
2019-07-14 02:20:41 +0200

    Added EmailConfiguration class

---
+++ Project/server/src/main/java/it/polito/ai/project/services/email/EmailConfiguration.java
@@ -0,0 +1,6 @@
+package it.polito.ai.project.services.email;
+
+public class EmailConfiguration {
+    public static final String FROM = "brucolini19@gmail.com";
+    public static final String BASE_URL = "http://localhost:8080";
+}
