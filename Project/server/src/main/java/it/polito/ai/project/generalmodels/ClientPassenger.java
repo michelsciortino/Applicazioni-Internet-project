7069a7c45bf1873cfb82bfbb003fd182a47202ea
MoraAndrea <s255280@studenti.polito.it>
2019-07-16 00:15:13 +0200

    Changed passenger class, added state, added stops

--- Project/server/src/main/java/it/polito/ai/project/generalmodels/ClientPassenger.java
+++ Project/server/src/main/java/it/polito/ai/project/generalmodels/ClientPassenger.java
@@ -1,17 +1,25 @@
 package it.polito.ai.project.generalmodels;
 
 import it.polito.ai.project.services.database.models.Child;
+import it.polito.ai.project.services.database.models.PassengerState;
+import it.polito.ai.project.services.database.models.PediStop;
 import lombok.Data;
 
 @Data
 public class ClientPassenger {
     private Child childDetails;
+    private PediStop stopReserved;
+    private PediStop stopTaken;
+    private PediStop stopDelivered;
     private boolean isReserved;
-    private boolean isPresent;
+    private PassengerState state;
 
-    public ClientPassenger(Child childDetails, boolean isReserved, boolean isPresent) {
+    public ClientPassenger(Child childDetails, PediStop stopReserved, PediStop stopTaken, PediStop stopDelivered, boolean isReserved, PassengerState state) {
         this.childDetails = childDetails;
+        this.stopReserved = stopReserved;
+        this.stopTaken = stopTaken;
+        this.stopDelivered = stopDelivered;
         this.isReserved = isReserved;
-        this.isPresent = isPresent;
+        this.state = state;
     }
 }
--- Project/server/src/main/java/it/polito/ai/project/services/database/models/Passenger.java
+++ Project/server/src/main/java/it/polito/ai/project/services/database/models/Passenger.java
@@ -5,12 +5,18 @@ import lombok.Data;
 @Data
 public class Passenger {
     private Child childDetails;
+    private PediStop stopReserved;
+    private PediStop stopTaken;
+    private PediStop stopDelivered;
     private boolean isReserved;
-    private boolean isPresent;
+    private PassengerState state;
 
-    public Passenger(Child childDetails, boolean isReserved, boolean isPresent) {
+    public Passenger(Child childDetails, PediStop stopReserved, PediStop stopTaken, PediStop stopDelivered, boolean isReserved, PassengerState state) {
         this.childDetails = childDetails;
+        this.stopReserved = stopReserved;
+        this.stopTaken = stopTaken;
+        this.stopDelivered = stopDelivered;
         this.isReserved = isReserved;
-        this.isPresent = isPresent;
+        this.state = state;
     }
 }
