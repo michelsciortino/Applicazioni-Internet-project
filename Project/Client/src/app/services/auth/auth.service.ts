ed8ce8dd5310a236b97eb98855f5a9056215e58a
MoraAndrea <s255280@studenti.polito.it>
2019-08-20 15:14:23 +0200

    minor fix

--- Project/Client/src/app/services/auth/auth.service.ts
+++ Project/Client/src/app/services/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Credentials } from '../../models/credentials';
-import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { HttpHeaders, HttpClient, HttpParams } from '@angular/common/http';
 
 import { map, catchError } from 'rxjs/operators';
 import { ConfirmMail } from 'src/app/models/confirmMail';
@@ -31,8 +31,15 @@ export class AuthService {
                             case 0:
                                 reject('Server unreachable');
                                 break;
+                            case 400:
+                                console.log(error)
+                                reject('Bad request: ' + error.error.message);
+                                break;
                             case 403:
                                 reject('Invalid credentials');
+                                break;
+                            case 404:
+                                reject('User Not Found');
                                 break;
                             default:
                                 reject('Unknown error');
@@ -49,14 +56,19 @@ export class AuthService {
 
     public recovery(recoveryMail: string) {
         const reqHeaders = new HttpHeaders({ ContentType: 'application/json' });
+        const reqParams = new HttpParams().set('token', 'TOKEN' );
         return new Promise((resolve, reject) =>
-            this.http.post(`${AuthService.authEndpoint}/recovery`, { mail: recoveryMail }, { headers: reqHeaders })
+            this.http.post(`${AuthService.authEndpoint}/recovery`, { mail: recoveryMail }, { headers: reqHeaders, params: reqParams })
                 .subscribe(
                     () => resolve(),
                     (error) => {
                         switch (error.status) {
                             case 0:
                                 reject('Server unreachable');
+                                break;
+                            case 400:
+                                console.log(error)
+                                reject('Bad request: ' + error.error.message);
                                 break;
                             case 403:
                                 reject('Invalid mail');
@@ -80,6 +92,10 @@ export class AuthService {
                             case 0:
                                 reject('Server unreachable');
                                 break;
+                            case 400:
+                                console.log(error)
+                                reject('Bad request: ' + error.error.message);
+                                break;
                             case 403:
                                 reject('Unauthorized');
                                 break;
@@ -93,14 +109,19 @@ export class AuthService {
 
     public confirmMail(confirmMail: ConfirmMail) {
         const reqHeaders = new HttpHeaders({ ContentType: 'application/json' });
+        const reqParams = new HttpParams().set('token', 'TOKEN' );
         return new Promise((resolve, reject) =>
-            this.http.post(`${AuthService.authEndpoint}/confirm`, confirmMail, { headers: reqHeaders })
+            this.http.post(`${AuthService.authEndpoint}/confirm`, confirmMail, { headers: reqHeaders, params: reqParams })
                 .subscribe(
                     () => resolve(),
                     (error) => {
                         switch (error.status) {
                             case 0:
                                 reject('Server unreachable');
+                                break;
+                            case 400:
+                                console.log(error)
+                                reject('Bad request: ' + error.error.message);
                                 break;
                             case 403:
                                 reject('Unauthorized');
