a3796e9d4968776e1f7f3008857f5ee365ede6cf
MoraAndrea <s255280@studenti.polito.it>
2019-09-09 01:41:59 +0200

    Edited BaseUrl

--- Project/server/src/main/java/it/polito/ai/project/services/email/EmailConfiguration.java
+++ Project/server/src/main/java/it/polito/ai/project/services/email/EmailConfiguration.java
@@ -2,5 +2,5 @@ package it.polito.ai.project.services.email;
 
 public class EmailConfiguration {
     public static final String FROM = "brucolini19@gmail.com";
-    public static final String BASE_URL = "http://localhost:4200";
+    public static final String BASE_URL = "http://localhost:4200/#";
 }
