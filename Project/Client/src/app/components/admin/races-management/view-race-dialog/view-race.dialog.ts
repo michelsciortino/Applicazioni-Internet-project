f7d1218e0e5b3ef6b21ffe62b70d3ced7acd353a
MoraAndrea <s255280@studenti.polito.it>
2019-09-20 11:34:01 +0200

    Minor fix

--- Project/Client/src/app/components/admin/races-management/view-race-dialog/view-race.dialog.ts
+++ Project/Client/src/app/components/admin/races-management/view-race-dialog/view-race.dialog.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ViewChild, AfterViewInit, OnDestroy, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import { ViewUserDialog } from '../../users-management/view-user-dialog/view-user.dialog';
-import { Race } from 'src/app/models/race';
+import Utils from 'src/app/utils/utils';
 
 
 @Component({
@@ -11,22 +10,11 @@ import { Race } from 'src/app/models/race';
 })
 export class ViewRaceDialog {
 
-    constructor(public dialogRef: MatDialogRef<ViewRaceDialog>, @Inject(MAT_DIALOG_DATA) public data: Race) {
+    constructor(public dialogRef: MatDialogRef<ViewRaceDialog>, @Inject(MAT_DIALOG_DATA) public data: any) { }
 
-        // console.log(data.race.date.toLocaleDateString())
-        // console.log(data.race.date.toLocaleTimeString())
-        //console.log(data.race.date.getMilliseconds()+companion.finalStop.delayInMillis
-        //let a = new Date(data.race.date.getTime()).toLocaleTimeString()
-    }
+    getTime = Utils.getTime;
 
-    getTime(millisec: number) {
-        var options = { hour: '2-digit', minute: '2-digit' };
-        return new Date(millisec).toLocaleTimeString([], options)
-    }
-
-    getTimeWithSecond(millisec: number) {
-        return new Date(millisec).toLocaleTimeString('it-IT')
-    }
+    getTimeWithSecond = Utils.getTimeWithSecond;
 
     onCancel(): void {
         this.dialogRef.close();
