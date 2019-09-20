860eda28a5b05adeb22672a00e832456abb042d6
MoraAndrea <s255280@studenti.polito.it>
2019-09-20 17:47:06 +0200

    Fix

---
+++ Project/Client/src/app/components/parent/parent.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { UserService } from 'src/app/services/user/user.service';
+import { UserInfo } from 'src/app/models/user';
+
+@Component({
+    selector: 'app-parent',
+    templateUrl: './parent.component.html',
+    styleUrls: ['./parent.component.css', '../common.css']
+})
+export class ParentComponent implements OnInit, OnDestroy {
+    isCompanion: boolean;
+    userSub: Subscription;
+
+    constructor(private authSvc: AuthService, private userSvc: UserService, private router: Router) {
+    }
+
+    ngOnInit() {
+        this.userSub = this.userSvc.getUserInfo().subscribe(
+            (info) => { if (info) this.isCompanion = UserInfo.prototype.isCompanion(info); }
+        )
+    }
+
+    ngOnDestroy() {
+        this.userSub.unsubscribe();
+    }
+}
