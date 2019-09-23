5a2e9b4c0d4850888b1fa58fe48f8830a0fae91d
MoraAndrea <s255280@studenti.polito.it>
2019-09-24 01:02:53 +0200

    Format code

--- Project/Client/src/app/components/account/children-setting/add-child-dialog/add-child.dialog.ts
+++ Project/Client/src/app/components/account/children-setting/add-child-dialog/add-child.dialog.ts
@@ -10,14 +10,14 @@ import { UserService } from 'src/app/services/user/user.service';
     styleUrls: ['./add-child.dialog.css']
 })
 export class AddChildDialog {
-    child: Child= new Child;    
+    child = new Child(null, null, null, null);
 
-    cf="MRONDR94C13B019P"
+    cf = "MRONDR94C13B019P"
 
-    constructor(public dialogRef: MatDialogRef<AddChildDialog>,private userSvc:UserService, @Inject(MAT_DIALOG_DATA) public data:any) {  
+    constructor(public dialogRef: MatDialogRef<AddChildDialog>, private userSvc: UserService, @Inject(MAT_DIALOG_DATA) public data: any) {
     }
-    
-    addChild():void{
+
+    addChild(): void {
         this.data.user.children.push(this.child);
         this.userSvc.updateUser(this.data.user);
         this.dialogRef.close();
@@ -27,17 +27,17 @@ export class AddChildDialog {
         this.dialogRef.close();
     }
 
-    isComplete(): boolean{
-        return this.child.name!=null && this.child.surname!=null && this.child.cf!=null;
+    isComplete(): boolean {
+        return this.child.name != null && this.child.surname != null && this.child.cf != null;
     }
 
-    checkCF(){
-        var re = new RegExp(/^[A-Z]{6}\d{2}[A-Z]\d{2}[A-Z]\d{3}[A-Z]$/,'g');
-        if(this.child.cf!=null){
-            var match=this.child.cf.match(re);
-            if(match==null)
+    checkCF() {
+        var re = new RegExp(/^[A-Z]{6}\d{2}[A-Z]\d{2}[A-Z]\d{3}[A-Z]$/, 'g');
+        if (this.child.cf != null) {
+            var match = this.child.cf.match(re);
+            if (match == null)
                 return false;
-            else 
+            else
                 return true;
         }
         return false;
