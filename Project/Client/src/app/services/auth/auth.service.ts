2eb56df0878cb424fd469c51ee5ad0ed65fddc79
michelsciortino <michel.sciortino@outlook.com>
2019-10-03 15:34:31 +0200

    Updating auth service

--- Project/Client/src/app/services/auth/auth.service.ts
+++ Project/Client/src/app/services/auth/auth.service.ts
@@ -34,7 +34,7 @@ export class AuthService {
     }
 
     public observeLoggedStatus(): Observable<boolean> {
-        return this.authSbj.asObservable().pipe(map((data) => (console.log(data),data != null)));
+        return this.authSbj.asObservable().pipe(map((data) => (/*console.log(data),*/data != null)));
     }
 
     //#endregion
@@ -42,37 +42,14 @@ export class AuthService {
     //#region Auth Methods
     public login(credentials: Credentials) {
         const reqHeaders = new HttpHeaders({ ContentType: 'application/json' });
-        return new Promise((resolve, reject) =>
-            this.http.post(`${AuthService.authEndpoint}/login`, credentials, { headers: reqHeaders })
-                .subscribe(
-                    (data: AuthenticatedUser) => {
-                        // handle jwt
-                        localStorage.setItem(AUTHENTICATED_USER, JSON.stringify(data));
-                        this.authSbj.next(data);
-                        // console.log("Login response:", JSON.stringify(this.authSbj.value));
-                        resolve();
-                    },
-                    (error) => {
-                        switch (error.status) {
-                            case 0:
-                                reject('Server unreachable');
-                                break;
-                            case 400:
-                                console.log(error);
-                                reject('Bad request: ' + error.error.message);
-                                break;
-                            case 403:
-                                reject('Invalid credentials');
-                                break;
-                            case 404:
-                                reject('User Not Found');
-                                break;
-                            default:
-                                reject('Unknown error');
-                                break;
-                        }
-                    }
-                )
+        return this.http.post(`${AuthService.authEndpoint}/login`, credentials, { headers: reqHeaders })
+        .toPromise()
+        .then( (data: AuthenticatedUser) => {
+                    // handle jwt
+                    localStorage.setItem(AUTHENTICATED_USER, JSON.stringify(data));
+                    this.authSbj.next(data);
+                    // console.log("Login response:", JSON.stringify(this.authSbj.value));
+                }
         );
     }
 
